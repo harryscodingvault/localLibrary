7a1dd3bf08426720e988c62a761f6205780e9e98
HaroldDRF <95robots@gmail.com>
2022-03-16 01:26:25 -0700

    getMostCommonGenres completed

--- Project_Local_library_1/public/src/home.js
+++ Project_Local_library_1/public/src/home.js
@@ -6,9 +6,30 @@ function getTotalAccountsCount(accounts) {
   return accounts.length;
 }
 
-function getBooksBorrowedCount(books) {}
+function getBooksBorrowedCount(books) {
+  const countBooks = books.filter((book) => book.borrows[0].returned === false);
+  return countBooks.length;
+}
 
-function getMostCommonGenres(books) {}
+function getMostCommonGenres(books) {
+  let allGenres = [];
+  for (let i in books) {
+    const genre = books[i].genre;
+
+    if (!allGenres.some((item) => item.name === genre)) {
+      allGenres.push({ name: genre, count: 1 });
+    } else {
+      const currIndex = allGenres.findIndex((element, index) => {
+        if (element.name === genre) {
+          return true;
+        }
+      });
+      allGenres[currIndex].count += 1;
+    }
+  }
+  const sortedGenres = allGenres.sort((a, b) => b.count - a.count);
+  return sortedGenres.slice(0, 5);
+}
 
 function getMostPopularBooks(books) {}
 
